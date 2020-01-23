3a5aadbbbe534b5ff87f51dd0eabd594992fc6a7
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-01-23 11:36:57 +0100

    Add files via upload

---
+++ nikŠimetićRadioBottunKontrola/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace nikŠimetićRadioBottunKontrola
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRezerviraj = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.radioButtonAvion = new System.Windows.Forms.RadioButton();
+            this.radioButtonAutobus = new System.Windows.Forms.RadioButton();
+            this.radioButttonVlastiti = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // buttonRezerviraj
+            // 
+            this.buttonRezerviraj.Location = new System.Drawing.Point(286, 219);
+            this.buttonRezerviraj.Name = "buttonRezerviraj";
+            this.buttonRezerviraj.Size = new System.Drawing.Size(174, 23);
+            this.buttonRezerviraj.TabIndex = 0;
+            this.buttonRezerviraj.Text = "Rezerviraj";
+            this.buttonRezerviraj.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(275, 286);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 89);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Odabrali ste prijevoz";
+            // 
+            // radioButtonAvion
+            // 
+            this.radioButtonAvion.AutoSize = true;
+            this.radioButtonAvion.Location = new System.Drawing.Point(286, 36);
+            this.radioButtonAvion.Name = "radioButtonAvion";
+            this.radioButtonAvion.Size = new System.Drawing.Size(52, 17);
+            this.radioButtonAvion.TabIndex = 2;
+            this.radioButtonAvion.TabStop = true;
+            this.radioButtonAvion.Text = "Avion";
+            this.radioButtonAvion.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonAutobus
+            // 
+            this.radioButtonAutobus.AutoSize = true;
+            this.radioButtonAutobus.Location = new System.Drawing.Point(286, 84);
+            this.radioButtonAutobus.Name = "radioButtonAutobus";
+            this.radioButtonAutobus.Size = new System.Drawing.Size(64, 17);
+            this.radioButtonAutobus.TabIndex = 3;
+            this.radioButtonAutobus.TabStop = true;
+            this.radioButtonAutobus.Text = "Autobus";
+            this.radioButtonAutobus.UseVisualStyleBackColor = true;
+            // 
+            // radioButttonVlastiti
+            // 
+            this.radioButttonVlastiti.AutoSize = true;
+            this.radioButttonVlastiti.Location = new System.Drawing.Point(286, 141);
+            this.radioButttonVlastiti.Name = "radioButttonVlastiti";
+            this.radioButttonVlastiti.Size = new System.Drawing.Size(95, 17);
+            this.radioButttonVlastiti.TabIndex = 4;
+            this.radioButttonVlastiti.TabStop = true;
+            this.radioButttonVlastiti.Text = "Vlastiti Prijevoz";
+            this.radioButttonVlastiti.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButttonVlastiti);
+            this.Controls.Add(this.radioButtonAutobus);
+            this.Controls.Add(this.radioButtonAvion);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonRezerviraj);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRezerviraj;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton radioButtonAvion;
+        private System.Windows.Forms.RadioButton radioButtonAutobus;
+        private System.Windows.Forms.RadioButton radioButttonVlastiti;
+    }
+}
+
